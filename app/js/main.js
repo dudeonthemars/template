f2affdd953832c7880f0ed49232cd2ea8a1e0466
ast_vplanka <v.planka@astoundcommerce.com>
2020-05-17 11:01:12 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,259 @@
+$(document).on('ready', function() {
+
+    //  WOW Transitions
+    if ($(window).width() > 767) {
+        wow = new WOW({
+            boxClass: 'wow', // default               
+            offset: 200, // default
+            mobile: true, // default
+            live: true // default
+        })
+        wow.init();
+        $(window).stellar();
+    }
+
+    // smooth scroll
+    $("body").on("click", "a.btn, a.animate, .btn-liquid, a.smooth", function() {
+        var idtop = $($(this).attr("href")).offset().top;
+        $('html,body').animate({
+            scrollTop: idtop
+        }, 600);
+        return false;
+    });
+
+    //text-rotator
+    $(".rotate").textrotator({
+        animation: "dissolve", //  Options dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+        separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+        speed: 2000 // How many milliseconds until the next word show.
+    });
+    //slick
+    $('.testimonials').slick({
+        draggable: true,
+        infinite: true,
+        dots: false,
+        autoplay: false,
+        speed: 900,
+        cssEase: 'ease-in-out',
+        lazyLoad: 'ondemand',
+        slidesToShow: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false,
+                    arrows: false,
+                    infinite: true,
+
+                }
+            },
+            {
+                breakpoint: 760,
+                settings: {
+                    slidesToShow: 1,
+                    dots: false,
+                    arrows: false,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    dots: false,
+                    arrows: false
+                }
+            }
+
+        ]
+    });
+
+    //readmore
+
+    $('.testimonials p').readmore({
+        speed: 300,
+        moreLink: '<a href="#">Читать полностью</a>',
+        lessLink: '<a href="#">Закрыть</a>',
+        collapsedHeight: 240
+    });
+
+    // site preloader 
+    $(window).load(function() {
+        $('#preloader').fadeOut('slow', function() {
+            $(this).remove();
+        });
+    });
+});
+
+
+//parallax
+if ($(window).width() > 767) {
+    var scene = document.getElementById('scene1');
+    var parallax = new Parallax(scene);
+
+    var scene = document.getElementById('scene2');
+    var parallax = new Parallax(scene);
+
+}
+
+
+// form fields
+function checkform(f) {
+    var line = document.getElementsByClassName('.some-form__line');
+    var errMSG = "";
+    // цикл ниже перебирает все элементы в объекте f, 
+    // переданном в качестве параметра
+    // функции, в данном случае - наша форма.            
+    for (var i = 0; i < f.elements.length; i++)
+    // если текущий элемент имеет атрибут required
+    // т.е. обязательный для заполнения
+        if (null != f.elements[i].getAttribute("required"))
+        // проверяем, заполнен ли он в форме
+            if (isEmpty(f.elements[i].value)) // пустой
+                errMSG += "  " + f.elements[i].name + "\n"; // формируем сообщение
+            // об ошибке, перечисляя 
+            // незаполненные поля
+            // если сообщение об ошибке не пусто,
+            // выводим его, и возвращаем false     
+    if ("" != errMSG) {
+        alert("Не заполнены обязательные поля:\n" + errMSG);
+        return false;
+    }
+
+    function isEmpty(str) {
+        for (var i = 0; i < str.length; i++)
+            if (" " != str.charAt(i))
+                return false;
+        return true;
+    }
+
+}
+
+// count-down digits
+let countDown = (function() {
+    return {
+        init: function() {
+            var a = 0;
+            $(window).on('scroll', function() {
+                var oTop = $('#count_down').offset().top - window.innerHeight;
+                if (a == 0 && $(window).scrollTop() > oTop) {
+                    $('.counter_value').each(function() {
+                        var $this = $(this),
+                            countTo = $this.attr('data-count');
+                        $({
+                            countNum: $this.text()
+                        }).animate({
+                            countNum: countTo
+                        }, {
+                            duration: 2000,
+                            easing: 'swing',
+                            step: function() {
+                                $this.text(Math.floor(this.countNum))
+                            },
+                            complete: function() {
+                                $this.text(this.countNum)
+                            }
+                        })
+                    });
+                    a = 1
+                }
+            })
+        }
+    }
+})();
+
+// ОТСЧЕТ	
+let timer = (function($) {
+    return {
+        init: function() {
+            'use strict';
+            $.countdown.regionalOptions.ru = {
+                labels: ['Лет', 'Месяцев', 'Недель', 'Дней', 'Часов', 'Минут', 'Секунд'],
+                labels1: ['Год', 'Месяц', 'Неделя', 'День', 'Час', 'Минута', 'Секунда'],
+                labels2: ['Года', 'Месяца', 'Недели', 'Дня', 'Часа', 'Минуты', 'Секунды'],
+                compactLabels: ['л', 'м', 'н', 'д'],
+                compactLabels1: ['г', 'м', 'н', 'д'],
+                whichLabels: function(amount) {
+                    var units = amount % 10;
+                    var tens = Math.floor((amount % 100) / 10);
+                    return (amount === 1 ? 1 : (units >= 2 && units <= 4 && tens !== 1 ? 2 :
+                        (units === 1 && tens !== 1 ? 1 : 0)));
+                },
+                digits: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+                timeSeparator: ':',
+                isRTL: false
+            };
+            $.countdown.setDefaults($.countdown.regionalOptions.ru);
+            if (Cookies.get("timer") === undefined) { Cookies.set("timer", new Date(), { expires: 7 }) }
+            var now = new Date();
+            var timer = new Date(Cookies.get("timer"));
+            timer.setMilliseconds(59 * 60 * 60 * 1000); //hours
+            //timer = new Date(2019, 2 - 1, 8); // date 2019-year feb 8-day
+            $('#counter div').countdown({
+                until: timer,
+                format: 'dHMS',
+                /*onTick: showPauseTime*/
+                onExpiry: noDiscount
+            });
+
+            if (timer.getTime() < now.getTime()) { noDiscount() }
+
+            function noDiscount() {
+                console.log('some function')
+            }
+        }
+    }
+})(jQuery);
+
+
+// toggler 
+let toggler = (function() {
+    return {
+        init: function() {
+
+            let acc = document.getElementsByClassName("question");
+
+            for (let i = 0; i < acc.length; i++) {
+                acc[i].addEventListener('click', function() {
+                    this.classList.toggle('active');
+                    let panel = this.nextElementSibling;
+
+                    if (panel.style.opacity === "1") {
+                        panel.style.opacity = "0";
+                    } else {
+                        panel.style.opacity = "1";
+                    }
+                });
+            }
+        }
+    }
+})();
+
+// autodateChanger
+let dateChanger = (function() {
+    return {
+        init: function() {
+            let ourDate = jQuery('.ourDate');
+            let theDate = new Date(2019, 9, 14);
+            let currentDate = new Date();
+            let options = {
+                month: 'long',
+                day: 'numeric'
+            };
+
+            do {
+                theDate.setDate(theDate.getDate() + 14);
+            } while (currentDate > theDate);
+
+            for (let i = 0; i <= ourDate.length; i++) {
+                ourDate[i].innerHTML = theDate.toLocaleString("ru", options);
+            }
+
+        }
+    }
+})();
+// call need scripts
+countDown.init();
+timer.init();
+toggler.init();
+dateChanger.init();
---
+++ gulpfile.js
@@ -0,0 +1,145 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglifyjs'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    cache = require('gulp-cache'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cmq = require('crlab-gulp-combine-media-queries');
+	cheerio = require('gulp-cheerio');
+	svgmin = require('gulp-svgmin');
+	replace = require('gulp-replace');
+	svgSprite = require('gulp-svg-sprite');
+
+
+gulp.task('svg', ()=> {
+	return gulp.src('app/img/svg/*.svg')
+	.pipe(svgmin({
+		js2svg: {
+			pretty: true
+		}
+	}))
+	.pipe(cheerio({
+		run: function($) {
+			$('[fill]').removeAttr('fill');
+			$('[stroke]').removeAttr('stroke');
+			$('[style]').removeAttr('style');
+			$('text').remove();
+		},
+		parserOptions: {xmlMode: true}
+	}))
+	.pipe(replace('&gt;', '>'))
+	.pipe(svgSprite({
+		mode: {
+			symbol: {
+				sprite: "sprite.svg"
+			}
+		}
+	}))
+	.pipe(gulp.dest('app/img/svg/'));
+	
+});
+
+gulp.task('sass', function() {
+    return gulp.src('app/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(cmq())
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false
+    });
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([
+            'app/libs/slick-carousel/slick/slick.min.js',
+            'app/libs/jquery.scrollTo/jquery.scrollTo.min.js',
+            //'app/libs/rotator/jquery.simple-text-rotator.js',
+            //'app/libs/stellar/jquery.stellar.js',
+            'app/libs/timer-keithwood/timer-keithwood.js',
+            //'app/libs/wow/dist/wow.min.js',
+            'app/libs/readmore-js/readmore.min.js'
+        ])
+        .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
+});
+
+gulp.task('code', function() {
+    return gulp.src('app/*.html')
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('mainjs', function() {
+    return gulp.src('app/js/main.js')
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+
+gulp.task('css-libs', function() {
+    return gulp.src('app/sass/libs.scss') // Выбираем файл для минификации
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(cssnano()) // Сжимаем
+        .pipe(rename({ suffix: '.min' })) // Добавляем суффикс .min
+        .pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
+});
+
+gulp.task('clean', async function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('img', function() {
+    return gulp.src('app/img/**/*') // Берем все изображения из app
+        .pipe(cache(imagemin({ // С кешированием
+            // .pipe(imagemin({ // Сжимаем изображения без кеширования
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            use: [pngquant()]
+        })) /**/ )
+        .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+});
+
+gulp.task('prebuild', async function() {
+
+    var buildCss = gulp.src([ // Переносим библиотеки в продакшен
+            'app/css/style.css',
+            'app/css/libs.min.css'
+        ])
+        .pipe(gulp.dest('dist/css'))
+
+    var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+        .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+        .pipe(gulp.dest('dist/js'))
+
+    var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+        .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('clear', function(callback) {
+    return cache.clearAll();
+})
+
+gulp.task('watch', function() {
+    gulp.watch('app/sass/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('app/*.html', gulp.parallel('code'));
+    gulp.watch(['app/js/main.js', 'app/libs/**/*.js'], gulp.parallel('scripts'));
+});
+gulp.task('default', gulp.parallel('css-libs', 'code', 'mainjs', 'sass', 'scripts', 'browser-sync', 'watch'));
+gulp.task('build', gulp.parallel('prebuild', 'clean', 'img', 'sass', 'scripts'));
